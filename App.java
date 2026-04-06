5a9eab77ed9c0d034c17a0d0dd0afc9f4bcfcf1a
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-06 12:17:14 +0530

    Initial Java CI/CD commit

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+ public static void main(String[] args) throws Exception {
+ System.out.println("Five events during Rivera....");
+ while (true) {
+ Thread.sleep(5000);
+ System.out.println("1.Pro show");
+ }
+ }
+}
